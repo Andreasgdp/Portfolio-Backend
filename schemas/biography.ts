afa717b01b5373b9e461e58496430969f3fa6153
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-04-21 18:49:09 +0200

    added biography

---
+++ schemas/biography.ts
@@ -0,0 +1,17 @@
+export default {
+	name: 'biography',
+	title: 'Biography',
+	type: 'document',
+	fields: [
+		{
+			name: 'year',
+			title: 'Year',
+			type: 'string',
+		},
+		{
+			name: 'content',
+			title: 'Content',
+			type: 'string',
+		},
+	],
+};
